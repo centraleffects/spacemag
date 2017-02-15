5cf7927b97a12be646716a5333b7bf3705e37242
Dexter Bengil <dexterb2992@gmail.com>
2017-02-16 00:35:03 +0800

    published authentication files

--- app/User.php
+++ app/User.php
@@ -9,14 +9,8 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = [ 'id' ];
+    protected $table = "users";   
 
     /**
      * The attributes that should be hidden for arrays.
@@ -26,4 +20,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('facebook_id')->nullable();
+            $table->string('type')->default('customer');
+            $table->string('confirmation_code')->nullable();
+            $table->integer('newsletter_subscription')->default(0);
+            $table->string('last_online')->nullable();
+            $table->string('address')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('mobile')->nullable();
+            $table->string('social_security_id')->nullable();
+            $table->string('lang')->default('se'); // defaults to Swedish
+            
             $table->rememberToken();
             $table->timestamps();
         });
