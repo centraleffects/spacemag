8fbb492d23785ab2d5eeac10c08b6e4c0ff0748c
Rex Bengil <centraleffects@yahoo.com>
2017-03-08 22:41:59 +0800

    Updated color scheme

--- resources/views/layouts/_partials/sidebar.blade.php
+++ resources/views/layouts/_partials/sidebar.blade.php
@@ -1,5 +1,5 @@
-<section class="nav-sidebar side-nav" id="slide-out">
-  <div class="row brand">
+<section class="nav-sidebar side-nav blue lighten-2" id="slide-out">
+  <div class="row brand blue lighten-2">
        <img src="/images/rebuy_logo.png" class="img-responsive logo">
   </div>
   <div class="row">
