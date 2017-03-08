a3ebc896157ef6a003081df9228d1d73435fbce8
Rex Bengil <centraleffects@yahoo.com>
2017-03-08 22:29:35 +0800

    Updated dashboard design for admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,44 @@
 @component('admin.layouts.app')
-	
+<div class="toolbar row">
+	<div class="col s12">
+		<nav>
+		    <div class="nav-wrapper">
+		      <ul id="nav-mobile" class="right hide-on-med-and-down">
+		         <li><a href="#">Add Shop Owner</a></li>
+				 <li><a href="#">List Show Owners</a></li>
+				 <li><a href="#">List Shops</a></li>
+		      </ul>
+		    </div>
+		</nav>
+	</div>
+</div>
+<div class="row content-wrap">
+
+	<div class="col s3" id="dashleft-sidebar">
+		<h5><i class="fa fa-caret-down" aria-hidden="true"></i> List of Shops</h5>
+		<ul class="collection">
+			<li class="collection-item">
+				<svg class="svgIcon itemRow-dragIcon" viewBox="0 0 32 32" title="drag handle"><path d="M 14 5.5 a 3 3 0 1 1 -3 -3 A 3 3 0 0 1 14 5.5 Z m 7 3 a 3 3 0 1 0 -3 -3 A 3 3 0 0 0 21 8.5 Z m -10 4 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 12.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 12.5 Z m -10 10 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 22.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 22.5 Z"></path></svg>
+				<input type="text" name="shop_name" value="Bunny Jeans">
+			</li>
+			<li class="collection-item">
+				<svg class="svgIcon itemRow-dragIcon" viewBox="0 0 32 32" title="drag handle"><path d="M 14 5.5 a 3 3 0 1 1 -3 -3 A 3 3 0 0 1 14 5.5 Z m 7 3 a 3 3 0 1 0 -3 -3 A 3 3 0 0 0 21 8.5 Z m -10 4 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 12.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 12.5 Z m -10 10 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 22.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 22.5 Z"></path></svg>
+				<input type="text" name="shop_name" value="Playboy">
+			</li>
+			<li class="collection-item">
+				<svg class="svgIcon itemRow-dragIcon" viewBox="0 0 32 32" title="drag handle"><path d="M 14 5.5 a 3 3 0 1 1 -3 -3 A 3 3 0 0 1 14 5.5 Z m 7 3 a 3 3 0 1 0 -3 -3 A 3 3 0 0 0 21 8.5 Z m -10 4 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 12.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 12.5 Z m -10 10 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 22.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 22.5 Z"></path></svg>
+				<input type="text" name="shop_name" value="Vanilla Marketing">
+			</li>
+			<li class="collection-item">
+				<svg class="svgIcon itemRow-dragIcon" viewBox="0 0 32 32" title="drag handle"><path d="M 14 5.5 a 3 3 0 1 1 -3 -3 A 3 3 0 0 1 14 5.5 Z m 7 3 a 3 3 0 1 0 -3 -3 A 3 3 0 0 0 21 8.5 Z m -10 4 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 12.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 12.5 Z m -10 10 a 3 3 0 1 0 3 3 A 3 3 0 0 0 11 22.5 Z m 10 0 a 3 3 0 1 0 3 3 A 3 3 0 0 0 21 22.5 Z"></path></svg>
+				<input type="text" name="shop_name" value="Marco Jeans & Co.">
+			</li>
+		</ul>
+	</div>
+	<div class="col s6">
+	</div>
+	<div class="col s3">
+	</div>
+
+</div>
 @endcomponent
