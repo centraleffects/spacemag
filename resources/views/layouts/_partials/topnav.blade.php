f539eafaf83206b4cdabfce972efc340579cb64f
Rex Bengil <centraleffects@yahoo.com>
2017-05-06 01:21:29 +0800

    OWNER - added shop switch through dropdown menu

--- resources/views/layouts/_partials/topnav.blade.php
+++ resources/views/layouts/_partials/topnav.blade.php
@@ -3,14 +3,20 @@
 	  <ul class="right hide-on-small-and-down">
 	    @if (auth()->user()->role == "owner")
 		    <li><a class="dropdown-button" href="#!" data-activates="dropdown1">{{session()->get("selected_shop")->name}}<i class="material-icons right">arrow_drop_down</i></a></li>
-		    <li class="hide-on-med-and-down"><a href="#" class="do-nav-slideout"><i class="material-icons">menu</i></a></li>
+		@elseif (auth()->user()->role == "admin")    
+			 <li class="hide-on-med-and-down"><a href="">{{auth()->user()->first_name.' '.auth()->user()->last_name}}<i class="material-icons right">perm_identity</i></i></a></li>
+		@else
+
 	    @endif
+	    <li class="hide-on-med-and-down"><a href="#" class="do-nav-slideout"><i class="material-icons">menu</i></a></li>
 	  </ul>
 	  <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
 	</div>
 </nav>
 <ul id="dropdown1" class="dropdown-content">
-	@foreach (session()->get('shops') as $shop)
-		<li><a href="/shop/set/{{$shop->id}}?redirect={{base64_encode(Request::url())}}">{{$shop->name}}</a></li>
-	@endforeach
+	@if (!empty(session()->get('shops')))
+		@foreach (session()->get('shops') as $shop)
+			<li><a href="/shop/set/{{$shop->id}}?redirect={{base64_encode(Request::url())}}">{{$shop->name}}</a></li>
+		@endforeach
+	@endif
 </ul>
