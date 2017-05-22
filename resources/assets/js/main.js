ad3ee0c8fda45f8ab0ef910f150c03dd4596f503
Dexter Bengil <dexterb2992@gmail.com>
2017-05-22 20:21:52 +0800

    Fixed 'Tried to load angular more than once' error

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,5 +1,3 @@
-require('./bootstrap');	
-
 window.app = angular.module('rebuy', ["angucomplete-alt"], function ($httpProvider){
 	// Use x-www-form-urlencoded Content-Type
 	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
